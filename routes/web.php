262f2bc9d441d15f004a41400bae2f89e1cd43ab
datph50941 <acenguyen61@gmail.com>
2025-05-21 17:01:44 +0700

    Giao diện admin

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('clients.home');
-})->name('home');
-Route::get('/admin', function () {
-    return view('admin.index');
-})->name('admin.index');
+
