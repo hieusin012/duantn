5b42102a0f08104cadd8a7a8781c46b98fa59c75
datph50941 <acenguyen61@gmail.com>
2025-05-25 21:44:09 +0700

    Cap Nhat Admin

---
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -0,0 +1,70 @@
+<div class="sidebar" id="sidebar">
+    <script type="text/javascript">
+        try{ace.settings.check('sidebar' , 'fixed')}catch(e){}
+    </script>
+
+    <div class="sidebar-shortcuts" id="sidebar-shortcuts">
+        <div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
+            <button class="btn btn-small btn-success">
+                <i class="icon-signal"></i>
+            </button>
+
+            <button class="btn btn-small btn-info">
+                <i class="icon-pencil"></i>
+            </button>
+
+            <button class="btn btn-small btn-warning">
+                <i class="icon-group"></i>
+            </button>
+
+            <button class="btn btn-small btn-danger">
+                <i class="icon-cogs"></i>
+            </button>
+        </div>
+
+        <div class="sidebar-shortcuts-mini" id="sidebar-shortcuts-mini">
+            <span class="btn btn-success"></span>
+
+            <span class="btn btn-info"></span>
+
+            <span class="btn btn-warning"></span>
+
+            <span class="btn btn-danger"></span>
+        </div>
+    </div><!-- #sidebar-shortcuts -->
+
+    <ul class="nav nav-list">
+        <li class="active">
+            <a href="index.html">
+                <i class="icon-dashboard"></i>
+                <span class="menu-text"> Dashboard </span>
+            </a>
+        </li>
+        
+        <li>
+            <a href="typography.html">
+                <i class="fas fa-th-list"></i>
+                <span class="menu-text"> Categories </span>
+            </a>
+        </li>
+        
+        <li>
+            <a href="{{route('dash.index')}}">
+                <i class="fas fa-box-open"></i>
+                <span class="menu-text"> Products </span>
+            </a>
+        </li>
+        
+        
+        
+
+    </ul><!-- /.nav-list -->
+
+    <div class="sidebar-collapse" id="sidebar-collapse">
+        <i class="icon-double-angle-left" data-icon1="icon-double-angle-left" data-icon2="icon-double-angle-right"></i>
+    </div>
+
+    <script type="text/javascript">
+        try{ace.settings.check('sidebar' , 'collapsed')}catch(e){}
+    </script>
+</div>
