44dddd65e34eed43a65e464c47a35a533fb50f99
namkiem29 <namkiem0000@gmail.com>
2025-06-20 17:40:04 +0700

    Quên mật khẩu

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -50,33 +50,33 @@ class AuthController extends Controller
 
     // Xử lý đăng ký
     public function register(Request $request)
-{
-    $data = $request->validate([
-        'fullname' => ['required', 'string', 'max:255'],
-        'email' => ['required', 'email', 'unique:users,email'],
-        'password' => ['required', 'confirmed', 'min:6'],
-    ], [
-        'fullname.required' => 'Vui lòng nhập tên.',
-        'fullname.string' => 'Tên không hợp lệ.',
-        'fullname.max' => 'Tên không được vượt quá 255 ký tự.',
-        'email.required' => 'Vui lòng nhập email.',
-        'email.email' => 'Email không đúng định dạng.',
-        'email.unique' => 'Email đã được sử dụng.',
-        'password.required' => 'Vui lòng nhập mật khẩu.',
-        'password.confirmed' => 'Mật khẩu xác nhận không khớp.',
-        'password.min' => 'Mật khẩu phải có ít nhất 6 ký tự.',
-    ]);
+    {
+        $data = $request->validate([
+            'fullname' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'confirmed', 'min:6'],
+        ], [
+            'fullname.required' => 'Vui lòng nhập tên.',
+            'fullname.string' => 'Tên không hợp lệ.',
+            'fullname.max' => 'Tên không được vượt quá 255 ký tự.',
+            'email.required' => 'Vui lòng nhập email.',
+            'email.email' => 'Email không đúng định dạng.',
+            'email.unique' => 'Email đã được sử dụng.',
+            'password.required' => 'Vui lòng nhập mật khẩu.',
+            'password.confirmed' => 'Mật khẩu xác nhận không khớp.',
+            'password.min' => 'Mật khẩu phải có ít nhất 6 ký tự.',
+        ]);
 
-    User::create([
-        'fullname' => $data['fullname'],
-        'email' => $data['email'],
-        'password' => bcrypt($data['password']),
-        'role' => 'member',
-    ]);
-    
-    // ✅ Không login, mà redirect về login
-    return redirect()->route('login')->with('success', 'Đăng ký thành công! Vui lòng đăng nhập.');
-}
+        User::create([
+            'fullname' => $data['fullname'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+            'role' => 'member',
+        ]);
+
+        // ✅ Không login, mà redirect về login
+        return redirect()->route('login')->with('success', 'Đăng ký thành công! Vui lòng đăng nhập.');
+    }
 
 
     // Đăng xuất
@@ -90,4 +90,5 @@ class AuthController extends Controller
 
         return redirect('/home');
     }
+    
 }
