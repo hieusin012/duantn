01605a8f120fa96c45d3d5bc2f5ce5be351cfef7
hieusin012 <hieu.sin.0411@gmail.com>
2025-06-26 14:09:45 +0700

    san pham moi nhat + bai viet moi nhat

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
         $products = Product::latest()->take(8)->get();
 
         // // Lấy 8 sản phẩm bán chạy nhất (giả sử có cột 'sold' trong bảng products)
-        // $bestSellers = Product::orderBy('sold', 'desc')->take(8)->get();
+        $bestSellers = Product::orderBy('sold', 'desc')->take(8)->get();
         $blogs = Blog::where('status', 0)->latest()->take(3)->get(); // chỉ bài đăng công khai
        return view('clients.home', compact('products','blogs'));
     }
