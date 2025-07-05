85acf50fb88caf2501cbe4d4bc8656f922bee1ca
hieusin012 <hieu.sin.0411@gmail.com>
2025-07-05 17:15:06 +0700

    cap nhap thanh toan

--- resources/views/clients/checkout.blade.php
+++ resources/views/clients/checkout.blade.php
@@ -82,6 +82,7 @@
     <label class="form-check-label" for="paymentQr">
         Thanh toán bằng mã QR (Chuyển khoản ngân hàng)
     </label>
+    
 </div>
                         <div class="form-check mb-2">
                             <input class="form-check-input" type="radio" name="payment" id="paymentVnPay" value="Thanh toán qua VNPay" {{ old('payment') == 'Thanh toán qua VNPay' ? 'checked' : '' }} required>
