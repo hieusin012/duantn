7930b519b274e649a337d888b08738382381c8a7
Vuong Quoc Phuong <phuongvqph51245@gmail.com>
2025-07-10 23:13:08 +0700

    Update nhap hang

--- app/Models/ImportDetail.php
+++ app/Models/ImportDetail.php
@@ -9,14 +9,15 @@ class ImportDetail extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['import_id', 'product_id', 'quantity', 'price', 'total_price'];
+    protected $fillable = ['import_id', 'variant_id', 'quantity', 'price', 'total_price'];
 
     public function import() {
         return $this->belongsTo(Import::class);
     }
 
-    public function product() {
-        return $this->belongsTo(Product::class);
+    public function variant() {
+        return $this->belongsTo(ProductVariant::class, 'variant_id');
     }
 }
+
 
