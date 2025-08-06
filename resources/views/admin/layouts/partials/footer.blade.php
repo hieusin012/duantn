2afd4ee6e8244b0675401b5ed99dafa705aeae00
trandinhtai1512204 <taitran1524@gmail.com>
2025-08-06 15:56:16 +0700

    update frontend

--- resources/views/admin/layouts/partials/footer.blade.php
+++ resources/views/admin/layouts/partials/footer.blade.php
@@ -1,8 +1,8 @@
-<footer style="position: fixed; bottom: 0; left: 0; width: 100%;
-               background: linear-gradient(90deg, #212529, #343a40);"
-        class="py-3 text-center text-white shadow">
+<footer class="footer-custom py-3 text-center text-white shadow">
+
+
     <p class="mb-0 fw-semibold" style="font-size: 14px;">
         &copy; <script>document.write(new Date().getFullYear());</script>
-        Phần mềm quản lý bán hàng | Dev by <span class="text-warning">Dat</span>
+        Thời Trang SportBay | Dev by <span class="text-warning">Admin</span>
     </p>
 </footer>
